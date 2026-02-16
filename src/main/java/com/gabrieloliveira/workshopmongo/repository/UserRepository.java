387e826747740aef3cb8df244427537df095d61d
Gabriel Alberto de Oliveira <gabrielalbertodeoliveira17@gmail.com>
2026-02-15 23:21:13 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/gabrieloliveira/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.gabrieloliveira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrieloliveira.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+}
