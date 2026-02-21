b69db354059bf352e7d1e6fdf71f8d36b100a056
Gabriel Alberto de Oliveira <gabrielalbertodeoliveira17@gmail.com>
2026-02-21 00:48:13 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gabrieloliveira/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrieloliveira/workshopmongo/services/UserService.java
@@ -1,7 +1,6 @@
 package com.gabrieloliveira.workshopmongo.services;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +29,12 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
